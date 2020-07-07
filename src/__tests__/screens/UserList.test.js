b2e6e417dde171adde45780af0e71be6cce1a3f6
Adithya Ajith <you@example.com>
2020-07-07 12:45:55 +0530

    test changes

--- src/__tests__/screens/UserList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import UsersList from '../../screens/UsersList';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<UsersList />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // for CRUD operations we are making use of a json-server hosted using a tunnelling service ngrok
-    baseURL: "http://b48087e6c721.ngrok.io"
+    baseURL: "http://5bac0657afe6.ngrok.io"
 });
