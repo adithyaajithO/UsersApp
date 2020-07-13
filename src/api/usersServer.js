0775c7779e04e7750bf6074c99122e47c1549582
Adithya Ajith <you@example.com>
2020-07-13 10:35:14 +0530

    Backend commit

--- src/api/usersServer.js
+++ src/api/usersServer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // for CRUD operations we are making use of a json-server hosted using a tunnelling service ngrok
+    // for CRUD operations we are making use of a node server using express framework making use of a tunnelling service 'ngrok'
     baseURL: "http://f6ee34ac8b47.ngrok.io"
 });
